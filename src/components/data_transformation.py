af363fd4342400bee117be69dd39b1caeb9558df
Taskmaster-1 <vivekyad5223@gmail.com>
2024-07-03 12:41:07 +0530

    Model Trainer

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -16,7 +16,7 @@ from src.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
-    preprocessor_obj_file_path=os.path.join('artifact',"preprocessor.pkl")
+    preprocessor_obj_file_path=os.path.join('artifact',"proprocessor.pkl")
 
 class DataTransformation:
     def __init__(self):
